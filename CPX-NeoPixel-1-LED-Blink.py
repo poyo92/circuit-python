c95b2d203ce78f139d44525e1f5780ff7f6b2bcc
Poyo:) <peevescoonterraaas@gmail.com>
2026-01-27 07:46:21 -0800

    Add NeoPixel LED blinking and rainbow effects

---
+++ CPX-NeoPixel-1-LED-Blink.py
@@ -0,0 +1,77 @@
+import time
+import board
+from rainbowio import colorwheel
+import neopixel
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.2, auto_write=False)
+
+color_chase_demo = 1
+flash_demo = 1
+rainbow_demo = 1
+rainbow_cycle_demo = 1
+
+
+def color_chase(color, wait):
+    for i in range(10):
+        pixels[i] = color
+        time.sleep(wait)
+        pixels.show()
+    time.sleep(0.5)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(10):
+            rc_index = (i * 256 // 10) + j * 5
+            pixels[i] = colorwheel(rc_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+
+def rainbow(wait):
+    for j in range(255):
+        for i in range(len(pixels)):
+            idx = int(i + j)
+            pixels[i] = colorwheel(idx & 255)
+        pixels.show()
+        time.sleep(wait)
+
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+WHITE = (255, 255, 255)
+OFF = (0, 0, 0)
+
+while True:
+    if color_chase_demo:
+        color_chase(RED, 0.1) 
+        color_chase(YELLOW, 0.1)
+        color_chase(GREEN, 0.1)
+        color_chase(CYAN, 0.1)
+        color_chase(BLUE, 0.1)
+        color_chase(PURPLE, 0.1)
+        color_chase(OFF, 0.1)
+
+    if flash_demo:
+        pixels.fill(RED)
+        pixels.show()
+        time.sleep(1)
+        pixels.fill(GREEN)
+        pixels.show()
+        time.sleep(1)
+        pixels.fill(BLUE)
+        pixels.show()
+        time.sleep(1)
+        pixels.fill(WHITE)
+        pixels.show()
+        time.sleep(1)
+
+    if rainbow_cycle_demo:
+        rainbow_cycle(0.05) 
+
+    if rainbow_demo:
+        rainbow(0.05) 
